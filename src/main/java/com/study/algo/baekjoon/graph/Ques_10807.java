0763896bdd6c6d593bfe6a13998bec464252159f
Jinseok Jang <hya2089@gmail.com>
2025-01-17 00:03:46 +0900

    [01.16] baekjoon 10807, 1316

---
+++ src/main/java/com/study/algo/baekjoon/graph/Ques_10807.java
@@ -0,0 +1,24 @@
+package com.study.algo.baekjoon.graph;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Ques_10807 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        if(arr.length != N) return;
+
+        int v = Integer.parseInt(br.readLine());
+
+        int count = (int) Arrays.stream(arr).filter(s -> s == v).count();
+        System.out.println(count);
+
+        br.close();
+    }
+}
