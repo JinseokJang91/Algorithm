c6d6f2cd0cd9aad27151b16960b150befc2756f1
Jinseok Jang <hya2089@gmail.com>
2025-01-18 21:54:52 +0900

    [01.18] baekjoon 2562

---
+++ src/main/java/com/study/algo/baekjoon/graph/Ques_2562.java
@@ -0,0 +1,31 @@
+package com.study.algo.baekjoon.graph;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Ques_2562 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] arr = new int[9];
+
+        for(int i = 0; i < 9; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        int max = Arrays.stream(arr).max().getAsInt();
+        int index = 0;
+        for(int i = 0; i < 9; i++) {
+            if(arr[i] == max) {
+                index = i + 1;
+                break;
+            }
+        }
+
+        System.out.println(max + "\n" + index);
+
+        br.close();
+    }
+}
