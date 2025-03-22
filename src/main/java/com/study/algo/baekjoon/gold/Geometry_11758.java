1da7c2edbefc4319f8a3149f61c4fa6fd085ea19
Jinseok Jang <hya2089@gmail.com>
2025-03-23 00:19:05 +0900

    [03.23] baekjoon 11758 CCW

--- src/main/java/com/study/algo/baekjoon/gold/Geometry_11758.java
+++ src/main/java/com/study/algo/baekjoon/gold/Geometry_11758.java
@@ -8,187 +8,51 @@ public class Geometry_11758 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        // 2차원 평면 위 좌표 P1, P2, P3를 이은 선분의 방향 : 시계(-1), 반시계(1), 일직선(0)
-        // P1(x1,y1), P2(x2,y2), P3(x3,y3)이라 가정하면
-        // P1, P2 간 변화인 dy/dx = y2-y1 / x2-x1 임
-        // P3가 dy/dx를 사용해서 구했을 때 도출되는 값이면 일직선
+        // 기하학 알고리즘 CCW(CounterClockWise)
+        // 벡터의 외적 활용 (P1:A, P2:B, P3:C 가정)
+        // A -> B -> C 순서로 발생하므로 벡터는 아래와 같이 생성
+        // ------------------
+        // ->  ->
+        // AB  BC
+        // ------------------
+        // => 벡터 AB와 벡터 BC 외적 진행 (양수 : 반시계, 음수 : 시계, 0 : 일직선)
 
-        // 1. 일직선
-        // (1) x1 = x2 = x3
-        // (2) y1 = y2 = y3
-        // (3) dy/dx 적용 시 도출 가능값
-        // 2. 시계 (기본 형태는 8가지, 세부 방향 나누면 케이스 별 3가지 => 총 24가지)
-        // (1) y2 - y1 > 0 && x2 - x1 = 0 >>> x3 - x2 > 0
-        // (2) y2 - y1 > 0 && x2 - x1 > 0 >>> x3 - x2 > 0 또는 y3 - y2 < 0
-        // (3) y2 - y1 = 0 && x2 - x1 > 0 >>> y3 - y2 < 0
-        // (4) y2 - y1 < 0 && x2 - x1 > 0 >>> x3 - x2 < 0 또는 y3 - y2 < 0
-        // (5) y2 - y1 < 0 && x2 - x1 = 0 >>> x3 - x2 < 0
-        // (6) y2 - y1 = 0 && x2 - x1 < 0 >>> y3 - y2 > 0
-        // (7) y2 - y1 < 0 && x2 - x1 < 0 >>> x3 - x2 < 0 또는 y3 - y2 > 0
-        // (8) y2 - y1 > 0 && x2 - x1 < 0 >>> x3 - x2 > 0 또는 y3 - y2 > 0
-        // 3. 반시계
-        // (1) y2 - y1 > 0 && x2 - x1 = 0 >>> x3 - x2 < 0
-        // (2) y2 - y1 > 0 && x2 - x1 > 0 >>> x3 - x2 < 0 또는 y3 - y2 > 0
-        // (3) y2 - y1 = 0 && x2 - x1 > 0 >>> y3 - y2 > 0
-        // (4) y2 - y1 < 0 && x2 - x1 > 0 >>> x3 - x2 > 0 또는 y3 - y2 > 0
-        // (5) y2 - y1 < 0 && x2 - x1 = 0 >>> x3 - x2 > 0
-        // (6) y2 - y1 = 0 && x2 - x1 < 0 >>> y3 - y2 < 0
-        // (7) y2 - y1 < 0 && x2 - x1 < 0 >>> x3 - x2 > 0 또는 y3 - y2 < 0
-        // (8) y2 - y1 > 0 && x2 - x1 < 0 >>> x3 - x2 < 0 또는 y3 - y2 < 0
+        // e.g. 예제 1 (1,1), (5,5), (7,3)
+        // 벡터 AB : (5-1, 5-1) = (4, 4)
+        // 벡터 BC : (7-5, 3-5) = (2, -2)
+        // 벡터 AB X 벡터 BC
+        // | a b | => | (x2-x1) (y2-y1) | => | 4  4 |
+        // | c d |    | (x3-x2) (y3-y2) |    | 2 -2 |
+        // => a*d - b*c = (y3-y2)*(x2-x1) - (x3-x2)*(y2-y1) = 4*-2 - 4*2 = -16
+
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        P P1 = new P(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+        P p1 = new P(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
 
         st = new StringTokenizer(br.readLine(), " ");
-        P P2 = new P(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+        P p2 = new P(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
 
         st = new StringTokenizer(br.readLine(), " ");
-        P P3 = new P(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+        P p3 = new P(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
 
-        System.out.println("P1 = " + P1);
-        System.out.println("P2 = " + P2);
-        System.out.println("P3 = " + P3);
+        // 공식 : (y3-y2)*(x2-x1) - (x3-x2)*(y2-y1)
+        int result = (p3.y - p2.y) * (p2.x - p1.x) - (p3.x - p2.x) * (p2.y - p1.y);
 
-        boolean isStraightLine = checkStraightLine(P1, P2, P3);
-        boolean isClockwise = checkClockwise(P1, P2, P3);
-        boolean isCounterClockwise = checkCounterClockwise(P1, P2, P3);
-        System.out.println("isStraightLine = " + isStraightLine);
-        System.out.println("isClockwise = " + isClockwise);
-        System.out.println("isCounterClockwise = " + isCounterClockwise);
+        if(result > 0) {
+            bw.write("1");
+        } else if(result < 0) {
+            bw.write("-1");
+        } else {
+            bw.write("0");
+        }
 
         bw.flush();
         bw.close();
         br.close();
     }
 
-    private static boolean checkCounterClockwise(P p1, P p2, P p3) {
-        // (1) y2 - y1 > 0 && x2 - x1 = 0 >>> x3 - x2 < 0
-        if(p2.y - p1.y > 0 && p2.x - p1.x == 0) {
-            if(p3.x - p2.x < 0) {
-                return true;
-            }
-        }
-        // (2) y2 - y1 > 0 && x2 - x1 > 0 >>> x3 - x2 < 0 또는 y3 - y2 > 0
-        else if(p2.y - p1.y > 0 && p2.x - p1.x > 0) {
-            if(p3.x - p2.x < 0 || p3.y - p2.y > 0) {
-                return true;
-            }
-        }
-        // (3) y2 - y1 = 0 && x2 - x1 > 0 >>> y3 - y2 > 0
-        else if(p2.y - p1.y == 0 && p2.x - p1.x > 0) {
-            if(p3.y - p2.y > 0) {
-                return true;
-            }
-        }
-        // (4) y2 - y1 < 0 && x2 - x1 > 0 >>> x3 - x2 > 0 또는 y3 - y2 > 0
-        else if(p2.y - p1.y < 0 && p2.x - p1.x > 0) {
-            if(p3.x - p2.x > 0 || p3.y - p2.y > 0) {
-                return true;
-            }
-        }
-        // (5) y2 - y1 < 0 && x2 - x1 = 0 >>> x3 - x2 > 0
-        else if(p2.y - p1.y < 0 && p2.x - p1.x == 0) {
-            if(p3.x - p2.x > 0) {
-                return true;
-            }
-        }
-        // (6) y2 - y1 = 0 && x2 - x1 < 0 >>> y3 - y2 < 0
-        else if(p2.y - p1.y == 0 && p2.x - p1.x < 0) {
-            if(p3.y - p2.y < 0) {
-                return true;
-            }
-        }
-        // (7) y2 - y1 < 0 && x2 - x1 < 0 >>> x3 - x2 > 0 또는 y3 - y2 < 0
-        else if(p2.y - p1.y < 0 && p2.x - p1.x < 0) {
-            if(p3.x - p2.x > 0 || p3.y - p2.y < 0) {
-                return true;
-            }
-        }
-        // (8) y2 - y1 > 0 && x2 - x1 < 0 >>> x3 - x2 < 0 또는 y3 - y2 < 0
-        else if(p2.y - p1.y > 0 && p2.x - p1.x < 0) {
-            if(p3.x - p2.x < 0 || p3.y - p2.y < 0) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    private static boolean checkClockwise(P p1, P p2, P p3) {
-        // (1) y2 - y1 > 0 && x2 - x1 = 0 >>> x3 - x2 > 0
-        if(p2.y - p1.y > 0 && p2.x - p1.x == 0) {
-            if(p3.x - p2.x > 0) {
-                return true;
-            }
-        }
-        // (2) y2 - y1 > 0 && x2 - x1 > 0 >>> x3 - x2 > 0 또는 y3 - y2 < 0
-        else if(p2.y - p1.y > 0 && p2.x - p1.x > 0) {
-            if(p3.x - p2.x > 0 || p3.y - p2.y < 0) {
-                return true;
-            }
-        }
-        // (3) y2 - y1 = 0 && x2 - x1 > 0 >>> y3 - y2 < 0
-        else if(p2.y - p1.y == 0 && p2.x - p1.x > 0) {
-            if(p3.y - p2.y < 0) {
-                return true;
-            }
-        }
-        // (4) y2 - y1 < 0 && x2 - x1 > 0 >>> x3 - x2 < 0 또는 y3 - y2 < 0
-        else if(p2.y - p1.y < 0 && p2.x - p1.x > 0) {
-            if(p3.x - p2.x < 0 || p3.y - p2.y < 0) {
-                return true;
-            }
-        }
-        // (5) y2 - y1 < 0 && x2 - x1 = 0 >>> x3 - x2 < 0
-        else if(p2.y - p1.y < 0 && p2.x - p1.x == 0) {
-            if(p3.x - p2.x < 0) {
-                return true;
-            }
-        }
-        // (6) y2 - y1 = 0 && x2 - x1 < 0 >>> y3 - y2 > 0
-        else if(p2.y - p1.y == 0 && p2.x - p1.x < 0) {
-            if(p3.y - p2.y > 0) {
-                return true;
-            }
-        }
-        // (7) y2 - y1 < 0 && x2 - x1 < 0 >>> x3 - x2 < 0 또는 y3 - y2 > 0
-        else if(p2.y - p1.y < 0 && p2.x - p1.x < 0) {
-            if(p3.x - p2.x < 0 || p3.y - p2.y > 0) {
-                return true;
-            }
-        }
-        // (8) y2 - y1 > 0 && x2 - x1 < 0 >>> x3 - x2 > 0 또는 y3 - y2 > 0
-        else if(p2.y - p1.y > 0 && p2.x - p1.x < 0) {
-            if(p3.x - p2.x > 0 || p3.y - p2.y > 0) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    private static boolean checkStraightLine(P p1, P p2, P p3) {
-        if(p1.x == p2.x && p2.x == p3.x) {
-            // 1. x1 = x2 = x3
-            return true;
-        } else if(p1.y == p2.y && p2.y == p3.y) {
-            // 2. y1 = y2 = y3
-            return true;
-        } else {
-            // 3. dy/dx 도출
-            int dydx = p2.y - p1.y / p2.x - p1.x;
-            int p3_y = p2.y + (p3.x - p2.x) * dydx;
-
-            if(p3_y == p3.y) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
     static class P {
-        private int x;
-        private int y;
+        private final int x;
+        private final int y;
 
         public P(int x, int y) {
             this.x = x;
---
+++ src/main/java/com/study/algo/baekjoon/gold/Geometry_11758_FAIL.java
@@ -0,0 +1,217 @@
+package com.study.algo.baekjoon.gold;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Geometry_11758_FAIL {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        // 2차원 평면 위 좌표 P1, P2, P3를 이은 선분의 방향 : 시계(-1), 반시계(1), 일직선(0)
+        // P1(x1,y1), P2(x2,y2), P3(x3,y3)이라 가정하면
+        // P1, P2 간 변화인 dy/dx = y2-y1 / x2-x1 임
+        // P3가 dy/dx를 사용해서 구했을 때 도출되는 값이면 일직선
+
+        // 1. 일직선
+        // (1) x1 = x2 = x3
+        // (2) y1 = y2 = y3
+        // (3) dy/dx 적용 시 도출 가능값
+        // 2. 시계 (기본 형태는 8가지, 세부 방향 나누면 케이스 별 3가지 => 총 24가지)
+        // (1) y2 - y1 > 0 && x2 - x1 = 0 >>> x3 - x2 > 0
+        // (2) y2 - y1 > 0 && x2 - x1 > 0 >>> x3 - x2 > 0 또는 y3 - y2 < 0
+        // (3) y2 - y1 = 0 && x2 - x1 > 0 >>> y3 - y2 < 0
+        // (4) y2 - y1 < 0 && x2 - x1 > 0 >>> x3 - x2 < 0 또는 y3 - y2 < 0
+        // (5) y2 - y1 < 0 && x2 - x1 = 0 >>> x3 - x2 < 0
+        // (6) y2 - y1 = 0 && x2 - x1 < 0 >>> y3 - y2 > 0
+        // (7) y2 - y1 < 0 && x2 - x1 < 0 >>> x3 - x2 < 0 또는 y3 - y2 > 0
+        // (8) y2 - y1 > 0 && x2 - x1 < 0 >>> x3 - x2 > 0 또는 y3 - y2 > 0
+        // 3. 반시계
+        // (1) y2 - y1 > 0 && x2 - x1 = 0 >>> x3 - x2 < 0
+        // (2) y2 - y1 > 0 && x2 - x1 > 0 >>> x3 - x2 < 0 또는 y3 - y2 > 0
+        // (3) y2 - y1 = 0 && x2 - x1 > 0 >>> y3 - y2 > 0
+        // (4) y2 - y1 < 0 && x2 - x1 > 0 >>> x3 - x2 > 0 또는 y3 - y2 > 0
+        // (5) y2 - y1 < 0 && x2 - x1 = 0 >>> x3 - x2 > 0
+        // (6) y2 - y1 = 0 && x2 - x1 < 0 >>> y3 - y2 < 0
+        // (7) y2 - y1 < 0 && x2 - x1 < 0 >>> x3 - x2 > 0 또는 y3 - y2 < 0
+        // (8) y2 - y1 > 0 && x2 - x1 < 0 >>> x3 - x2 < 0 또는 y3 - y2 < 0
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        P P1 = new P(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+
+        st = new StringTokenizer(br.readLine(), " ");
+        P P2 = new P(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+
+        st = new StringTokenizer(br.readLine(), " ");
+        P P3 = new P(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+
+//        System.out.println("P1 = " + P1);
+//        System.out.println("P2 = " + P2);
+//        System.out.println("P3 = " + P3);
+
+        boolean isStraightLine = checkStraightLine(P1, P2, P3);
+        boolean isClockwise = checkClockwise(P1, P2, P3);
+        boolean isCounterClockwise = checkCounterClockwise(P1, P2, P3);
+
+        if(isStraightLine) {
+            bw.write("0");
+        } else if(isClockwise) {
+            bw.write("-1");
+        } else if(isCounterClockwise) {
+            bw.write("1");
+        }
+
+//        System.out.println("isStraightLine = " + isStraightLine);
+//        System.out.println("isClockwise = " + isClockwise);
+//        System.out.println("isCounterClockwise = " + isCounterClockwise);
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    private static boolean checkCounterClockwise(P p1, P p2, P p3) {
+        // (1) y2 - y1 > 0 && x2 - x1 = 0 >>> x3 - x2 < 0
+        if(p2.y - p1.y > 0 && p2.x - p1.x == 0) {
+            if(p3.x - p2.x < 0) {
+                return true;
+            }
+        }
+        // (2) y2 - y1 > 0 && x2 - x1 > 0 >>> x3 - x2 < 0 또는 y3 - y2 > 0
+        else if(p2.y - p1.y > 0 && p2.x - p1.x > 0) {
+            if(p3.x - p2.x < 0 || p3.y - p2.y > 0) {
+                return true;
+            }
+        }
+        // (3) y2 - y1 = 0 && x2 - x1 > 0 >>> y3 - y2 > 0
+        else if(p2.y - p1.y == 0 && p2.x - p1.x > 0) {
+            if(p3.y - p2.y > 0) {
+                return true;
+            }
+        }
+        // (4) y2 - y1 < 0 && x2 - x1 > 0 >>> x3 - x2 > 0 또는 y3 - y2 > 0
+        else if(p2.y - p1.y < 0 && p2.x - p1.x > 0) {
+            if(p3.x - p2.x > 0 || p3.y - p2.y > 0) {
+                return true;
+            }
+        }
+        // (5) y2 - y1 < 0 && x2 - x1 = 0 >>> x3 - x2 > 0
+        else if(p2.y - p1.y < 0 && p2.x - p1.x == 0) {
+            if(p3.x - p2.x > 0) {
+                return true;
+            }
+        }
+        // (6) y2 - y1 = 0 && x2 - x1 < 0 >>> y3 - y2 < 0
+        else if(p2.y - p1.y == 0 && p2.x - p1.x < 0) {
+            if(p3.y - p2.y < 0) {
+                return true;
+            }
+        }
+        // (7) y2 - y1 < 0 && x2 - x1 < 0 >>> x3 - x2 > 0 또는 y3 - y2 < 0
+        else if(p2.y - p1.y < 0 && p2.x - p1.x < 0) {
+            if(p3.x - p2.x > 0 || p3.y - p2.y < 0) {
+                return true;
+            }
+        }
+        // (8) y2 - y1 > 0 && x2 - x1 < 0 >>> x3 - x2 < 0 또는 y3 - y2 < 0
+        else if(p2.y - p1.y > 0 && p2.x - p1.x < 0) {
+            if(p3.x - p2.x < 0 || p3.y - p2.y < 0) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private static boolean checkClockwise(P p1, P p2, P p3) {
+        // (1) y2 - y1 > 0 && x2 - x1 = 0 >>> x3 - x2 > 0
+        if(p2.y - p1.y > 0 && p2.x - p1.x == 0) {
+            if(p3.x - p2.x > 0) {
+                return true;
+            }
+        }
+        // (2) y2 - y1 > 0 && x2 - x1 > 0 >>> x3 - x2 > 0 또는 y3 - y2 < 0
+        else if(p2.y - p1.y > 0 && p2.x - p1.x > 0) {
+            if(p3.x - p2.x > 0 || p3.y - p2.y < 0) {
+                return true;
+            }
+        }
+        // (3) y2 - y1 = 0 && x2 - x1 > 0 >>> y3 - y2 < 0
+        else if(p2.y - p1.y == 0 && p2.x - p1.x > 0) {
+            if(p3.y - p2.y < 0) {
+                return true;
+            }
+        }
+        // (4) y2 - y1 < 0 && x2 - x1 > 0 >>> x3 - x2 < 0 또는 y3 - y2 < 0
+        else if(p2.y - p1.y < 0 && p2.x - p1.x > 0) {
+            if(p3.x - p2.x < 0 || p3.y - p2.y < 0) {
+                return true;
+            }
+        }
+        // (5) y2 - y1 < 0 && x2 - x1 = 0 >>> x3 - x2 < 0
+        else if(p2.y - p1.y < 0 && p2.x - p1.x == 0) {
+            if(p3.x - p2.x < 0) {
+                return true;
+            }
+        }
+        // (6) y2 - y1 = 0 && x2 - x1 < 0 >>> y3 - y2 > 0
+        else if(p2.y - p1.y == 0 && p2.x - p1.x < 0) {
+            if(p3.y - p2.y > 0) {
+                return true;
+            }
+        }
+        // (7) y2 - y1 < 0 && x2 - x1 < 0 >>> x3 - x2 < 0 또는 y3 - y2 > 0
+        else if(p2.y - p1.y < 0 && p2.x - p1.x < 0) {
+            if(p3.x - p2.x < 0 || p3.y - p2.y > 0) {
+                return true;
+            }
+        }
+        // (8) y2 - y1 > 0 && x2 - x1 < 0 >>> x3 - x2 > 0 또는 y3 - y2 > 0
+        else if(p2.y - p1.y > 0 && p2.x - p1.x < 0) {
+            if(p3.x - p2.x > 0 || p3.y - p2.y > 0) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private static boolean checkStraightLine(P p1, P p2, P p3) {
+        if(p1.x == p2.x && p2.x == p3.x) {
+            // 1. x1 = x2 = x3
+            return true;
+        } else if(p1.y == p2.y && p2.y == p3.y) {
+            // 2. y1 = y2 = y3
+            return true;
+        } else {
+            // 3. dy/dx 도출
+            int dydx = (p2.y - p1.y )/ (p2.x - p1.x);
+//            System.out.println("dydx = " + dydx);
+            int p3_y = p2.y + ((p3.x - p2.x) * dydx);
+//            System.out.println("p3_y = " + p3_y);
+
+            if(p3_y == p3.y) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    static class P {
+        private final int x;
+        private final int y;
+
+        public P(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public String toString() {
+            return "P{" +
+                    "x=" + x +
+                    ", y=" + y +
+                    '}';
+        }
+    }
+}
