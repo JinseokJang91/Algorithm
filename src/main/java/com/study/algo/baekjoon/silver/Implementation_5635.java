00a24aa7d085353a7f40e3d756bf4bb5a52908fc
Jinseok Jang <hya2089@gmail.com>
2025-04-05 00:17:08 +0900

    [04.04] baekjoon 5635

--- src/main/java/com/study/algo/baekjoon/silver/Implementation_5635.java
+++ src/main/java/com/study/algo/baekjoon/silver/Implementation_5635.java
@@ -16,6 +16,9 @@ public class Implementation_5635 {
         int n = Integer.parseInt(br.readLine());
         List<Person> persons = new ArrayList<>();
 
+        // 주어지는 정보 : 이름 일 월 년도
+        // 가장 나이가 적은 사람의 이름, 가장 나이가 많은 사람의 이름 출력
+
         StringTokenizer st = null;
         for(int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine(), " ");
@@ -28,20 +31,22 @@ public class Implementation_5635 {
             persons.add(new Person(name, day, month, year));
         }
 
+        // 년도 기준으로 오름차순 정렬 (년도는 작을수록 나이가 많음)
         Collections.sort(persons, (o1, o2) -> o1.year - o2.year);
 
         int oldestYear = persons.get(0).year;
         int youngestYear = persons.get(persons.size() - 1).year;
 
+        // 동일년도 존재 케이스 고려
         List<Person> oldPersons = persons.stream().filter(s -> s.year == oldestYear).collect(Collectors.toList());
         List<Person> youngPersons = persons.stream().filter(s -> s.year == youngestYear).collect(Collectors.toList());
 
-        System.out.println("youngPersons = " + youngPersons);
-        System.out.println("oldPersons = " + oldPersons);
-
+        // 1. 나이가 가장 적은 사람
         if(youngPersons.size() == 1) {
+            // 동일년도가 혼자면 바로 출력
             result.append(youngPersons.get(0).name).append("\n");
         } else {
+            // 동일년도가 둘 이상이면 월 => 일 순으로 비교
             int youngestMonth = youngPersons.get(0).month;
             int youngestDay = youngPersons.get(0).day;
             String youngestPersonName = youngPersons.get(0).name;
@@ -60,21 +65,21 @@ public class Implementation_5635 {
             result.append(youngestPersonName).append("\n");
         }
 
+        // 2. 나이가 가장 많은 사람
         if(oldPersons.size() == 1) {
+            // 동일년도가 혼자면 바로 출력
             result.append(oldPersons.get(0).name).append("\n");
         } else {
+            // 동일년도가 둘 이상이면 월 => 일 순으로 비교
             int oldestMonth = oldPersons.get(0).month;
             int oldestDay = oldPersons.get(0).day;
             String oldestPersonName = oldPersons.get(0).name;
             for(int i = 1; i < oldPersons.size(); i++) {
                 if(oldPersons.get(i).month < oldestMonth) {
                     oldestMonth = oldPersons.get(i).month;
+                    oldestDay = oldPersons.get(i).day;
                     oldestPersonName = oldPersons.get(i).name;
                 } else if(oldPersons.get(i).month == oldestMonth) {
-                    System.out.println("비교대상 = " + oldPersons.get(i).name);
-                    System.out.println("이전 = " + oldestPersonName);
-                    System.out.println(">> = " + oldPersons.get(i).day);
-                    System.out.println(">> = " + oldestDay);
                     if(oldPersons.get(i).day < oldestDay) {
                         oldestDay = oldPersons.get(i).day;
                         oldestPersonName = oldPersons.get(i).name;
@@ -93,10 +98,10 @@ public class Implementation_5635 {
     }
 
     static class Person {
-        private String name;
-        private int day;
-        private int month;
-        private int year;
+        private final String name;
+        private final int day;
+        private final int month;
+        private final int year;
 
         public Person(String name, int day, int month, int year) {
             this.name = name;
@@ -105,14 +110,15 @@ public class Implementation_5635 {
             this.year = year;
         }
 
-        @Override
-        public String toString() {
-            return "Person{" +
-                    "name='" + name + '\'' +
-                    ", day=" + day +
-                    ", month=" + month +
-                    ", year=" + year +
-                    '}';
-        }
+        // 로직 확인용
+//        @Override
+//        public String toString() {
+//            return "Person{" +
+//                    "name='" + name + '\'' +
+//                    ", day=" + day +
+//                    ", month=" + month +
+//                    ", year=" + year +
+//                    '}';
+//        }
     }
 }
