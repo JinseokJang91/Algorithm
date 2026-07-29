3a7679c30c108e32d902973453bd49423266e531
Jinseok Jang <hya2089@gmail.com>
2026-07-29 22:17:09 +0900

    [2026.07.29] Algorithm study

---
+++ src/main/java/com/study/algo/programmers/v20260625/V20260729.java
@@ -0,0 +1,22 @@
+package com.study.algo.programmers.v20260625;
+
+public class V20260729 {
+    public static void main(String[] args) {
+        int n1 = 98765;
+        int answer = solution(n1);
+        System.out.println("answer = " + answer);
+    }
+
+    // 피보나치 수
+    public static int solution(int n) {
+        long[] fiboArr = new long[n + 1];
+        fiboArr[0] = 0;
+        fiboArr[1] = 1;
+
+        for(int i = 2; i < fiboArr.length; i++) {
+            fiboArr[i] = (fiboArr[i - 2] + fiboArr[i - 1]) % 1234567;
+        }
+
+        return (int)fiboArr[n];
+    }
+}
